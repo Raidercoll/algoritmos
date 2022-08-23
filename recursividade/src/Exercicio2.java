c3fdb5650bd58b253c79ee837e40c5ae43f2d222
Davi Oliveira <103294834+Raidercoll@users.noreply.github.com>
2022-08-23 20:30:41 -0300

    Update Exercicio2.java

--- recursividade/src/Exercicio2.java
+++ recursividade/src/Exercicio2.java
@@ -1,12 +1,12 @@
-public class Exercicio2 {
+public class stringRevursiva {
+    public static String inverte(String str, int tam){
+        if(tam ==0) return ""+ str.charAt(tam) +"";
+        return str.charAt(tam) + inverte(str, tam-1);
 
-    public static String inverterString(String palavra){
-        if(palavra.length()==1){
-            return palavra;
-        }
-        
     }
+
     public static void main(String[] args) {
-        System.out.println(inverterString("oi"));
+
+        System.out.println(inverte("davi", "davi".length()-1));
     }
 }
